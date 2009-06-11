81adf915540c20514d233db9c33dfef3a846e8ac
curtismitchell <curtismitchell@gmail.com>
2009-06-10 21:10:28 -0400

    Changed assemblyinfo

--- WikiNotes/Properties/AssemblyInfo.cs
+++ WikiNotes/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WikiNotes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LexisNexis Group")]
+[assembly: AssemblyCompany("Vesource Corporation")]
 [assembly: AssemblyProduct("WikiNotes")]
-[assembly: AssemblyCopyright("Copyright © LexisNexis Group 2009")]
+[assembly: AssemblyCopyright("Copyright © Vesource Corporation 2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
