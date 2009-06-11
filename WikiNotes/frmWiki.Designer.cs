8be81a4f9b08758498cc059f6a0d7642d818ab41
curtismitchell <curtismitchell@gmail.com>
2009-06-10 20:09:30 -0400

    Added a panel to the form to contain the searchbox and previous button

--- WikiNotes/frmWiki.Designer.cs
+++ WikiNotes/frmWiki.Designer.cs
@@ -33,7 +33,9 @@
             this.rtbMain = new System.Windows.Forms.RichTextBox();
             this.btnPreviousPage = new System.Windows.Forms.Button();
             this.txtJump = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tlpMain.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tlpMain
@@ -43,14 +45,13 @@
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 188F));
             this.tlpMain.Controls.Add(this.rtbMain, 0, 0);
-            this.tlpMain.Controls.Add(this.btnPreviousPage, 0, 1);
-            this.tlpMain.Controls.Add(this.txtJump, 1, 1);
+            this.tlpMain.Controls.Add(this.panel1, 0, 1);
             this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpMain.Location = new System.Drawing.Point(0, 0);
             this.tlpMain.Name = "tlpMain";
             this.tlpMain.RowCount = 2;
             this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
             this.tlpMain.Size = new System.Drawing.Size(539, 391);
             this.tlpMain.TabIndex = 1;
             // 
@@ -63,7 +64,7 @@
             this.rtbMain.Location = new System.Drawing.Point(6, 6);
             this.rtbMain.Margin = new System.Windows.Forms.Padding(6);
             this.rtbMain.Name = "rtbMain";
-            this.rtbMain.Size = new System.Drawing.Size(527, 349);
+            this.rtbMain.Size = new System.Drawing.Size(527, 334);
             this.rtbMain.TabIndex = 0;
             this.rtbMain.Text = "";
             this.rtbMain.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.OnLinkClicked);
@@ -75,9 +76,9 @@
             this.btnPreviousPage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPreviousPage.BackgroundImage")));
             this.btnPreviousPage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnPreviousPage.Enabled = false;
-            this.btnPreviousPage.Location = new System.Drawing.Point(3, 364);
+            this.btnPreviousPage.Location = new System.Drawing.Point(2, 8);
             this.btnPreviousPage.Name = "btnPreviousPage";
-            this.btnPreviousPage.Size = new System.Drawing.Size(75, 24);
+            this.btnPreviousPage.Size = new System.Drawing.Size(75, 23);
             this.btnPreviousPage.TabIndex = 2;
             this.btnPreviousPage.Text = "Previous";
             this.btnPreviousPage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -86,18 +87,32 @@
             // 
             // txtJump
             // 
-            this.txtJump.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtJump.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.txtJump.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.txtJump.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.txtJump.BackColor = System.Drawing.Color.LightSkyBlue;
             this.txtJump.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtJump.ForeColor = System.Drawing.Color.Black;
-            this.txtJump.Location = new System.Drawing.Point(354, 366);
+            this.txtJump.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtJump.ForeColor = System.Drawing.Color.White;
+            this.txtJump.Location = new System.Drawing.Point(347, 8);
             this.txtJump.Name = "txtJump";
-            this.txtJump.Size = new System.Drawing.Size(182, 20);
+            this.txtJump.Size = new System.Drawing.Size(182, 21);
             this.txtJump.TabIndex = 3;
             this.txtJump.TextChanged += new System.EventHandler(this.txtJump_TextChanged);
             this.txtJump.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnJumpEntered);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.tlpMain.SetColumnSpan(this.panel1, 2);
+            this.panel1.Controls.Add(this.txtJump);
+            this.panel1.Controls.Add(this.btnPreviousPage);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 349);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(533, 39);
+            this.panel1.TabIndex = 4;
             // 
             // frmWiki
             // 
@@ -108,7 +123,8 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmWiki";
             this.tlpMain.ResumeLayout(false);
-            this.tlpMain.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -119,6 +135,7 @@
         private System.Windows.Forms.TableLayoutPanel tlpMain;
         private System.Windows.Forms.Button btnPreviousPage;
         private System.Windows.Forms.TextBox txtJump;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
